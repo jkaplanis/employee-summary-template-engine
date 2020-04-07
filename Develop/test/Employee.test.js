2d8cd8c96d83fdf658705b44494dc34e7a205e64
jkaplanis <johntkaplanis@gmail.com>
2020-04-07 16:23:16 -0700

    add logic to Engineer.js file

--- Develop/test/Employee.test.js
+++ Develop/test/Employee.test.js
@@ -2,31 +2,31 @@ const Employee = require("../lib/Employee");
 
 test("Can instantiate Employee instance", () => {
   const e = new Employee();
-  expect(typeof e).toBe("object"); //pass
+  expect(typeof e).toBe("object");
 });
 
 test("Can set name via constructor arguments", () => {
   const name = "Alice";
   const e = new Employee(name);
-  expect(e.name).toBe(name); //pass
+  expect(e.name).toBe(name);
 });
 
 test("Can set id via constructor argument", () => {
   const testValue = 100;
   const e = new Employee("Foo", testValue);
-  expect(e.id).toBe(testValue); //pass
+  expect(e.id).toBe(testValue);
 });
 
 test("Can set email via constructor argument", () => {
   const testValue = "test@test.com";
   const e = new Employee("Foo", 1, testValue);
-  expect(e.email).toBe(testValue); //pass
+  expect(e.email).toBe(testValue);
 });
 
 test("Can get name via getName()", () => {
   const testValue = "Alice";
   const e = new Employee(testValue);
-  expect(e.getName()).toBe(testValue); //pass
+  expect(e.getName()).toBe(testValue);
 });
 
 test("Can get id via getId()", () => {
