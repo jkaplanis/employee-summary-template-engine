6fd1497484868585858c3697dffb38aa6b44fce2
jkaplanis <johntkaplanis@gmail.com>
2020-04-07 16:30:25 -0700

    add logic for Intern.js

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,19 @@
-// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee.js");
+
+class Intern extends Employee {
+  constructor(name, id, email, school) {
+    super(name, id, email);
+    this.school = school;
+  }
+
+  getSchool() {
+    return this.school;
+  }
+
+  getRole() {
+    let role = "Intern";
+    return role;
+  }
+}
+
+module.exports = Intern;
