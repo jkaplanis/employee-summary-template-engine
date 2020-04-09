8641176b7601ef551205de72b4109c7b2f55911a
jkaplanis <johntkaplanis@gmail.com>
2020-04-08 18:15:31 -0700

    update and improve comments

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
+// Require Manager, Engineer, and Intern constructors
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
+// Require inquirer, path, and file system
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+// Require render function
+const render = require("./lib/htmlRenderer");
+
+// Allows for different OS path formatting
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
-
+// Create array to hold employee objects
 const employees = [];
 
+// Write file function checks if the output directory
+// is absent and creates it before proceeding to the
+// write file step.
 writeFile = html => {
   if (!fs.existsSync(OUTPUT_DIR)) {
     fs.mkdirSync(OUTPUT_DIR);
@@ -23,8 +32,9 @@ writeFile = html => {
     console.log(`Success! File location: ${outputPath}`);
   });
 };
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+
+// Inquirer prompt to determine which employee should be added next.
+// If the user is done, writefile is called.
 const addTeamMember = () => {
   inquirer
     .prompt([
@@ -52,9 +62,9 @@ const addTeamMember = () => {
       }
     });
 };
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+
+// Inquirer prompt to add manager.
+// Add team member function is called at the end.
 const addManager = () => {
   inquirer
     .prompt([
@@ -91,6 +101,8 @@ const addManager = () => {
     });
 };
 
+// Inquirer prompt to add Engineer.
+// Add team member function is called at the end.
 const addEngineer = () => {
   inquirer
     .prompt([
@@ -122,6 +134,8 @@ const addEngineer = () => {
     });
 };
 
+// Inquirer prompt to add Intern.
+// Add team member function is called at the end.
 const addIntern = () => {
   inquirer
     .prompt([
@@ -152,20 +166,6 @@ const addIntern = () => {
       addTeamMember();
     });
 };
+
+// Add Manger function called to start the program.
 addManager();
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
